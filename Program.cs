bd43ef6f2b31f6dcdd859eacdc260e21b909bb5b
Арутюнян Арман <arman.arutyunyan.1999@inbox.ru>
2023-03-16 13:59:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StahanovaAndSerbina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x: ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = (8 * Math.Sin(2 * x)) - Math.Cos(2 * x);
+            Console.WriteLine($"Значение F = {F}.");
+            Console.ReadKey();
+        }
+    }
+}
